ffcc8cd5172bc319a52642d95c3f8176adbfcd94
nadine525 <02_seeders_wide@icloud.com>
2023-07-15 18:39:55 +0200

    blacklist

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,7 @@ import storage from 'redux-persist/lib/storage';
 const contactsPersistConfig = {
   key: 'contacts',
   storage,
+  blacklist: ['filter'],
 };
 
 const reducers = combineReducers({
